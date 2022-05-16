433467eafe7bf906405ae55f86a18b19b210ea7c
Luyayeh <105659122+Luyayeh@users.noreply.github.com>
2022-05-16 09:38:09 -0700

    Add files via upload

---
+++ kMeans.py
@@ -0,0 +1,33 @@
+# Importing required modules
+
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+# Function to implement steps given in previous section
+def kmeans(x, k, no_of_iterations):
+    idx = np.random.choice(len(x), k, replace=False)
+    # Randomly choosing Centroids
+    centroids = x[idx, :]  # Step 1
+
+    # finding the distance between centroids and all the data points
+    distances = cdist(x, centroids, 'euclidean')  # Step 2
+
+    # Centroid with the minimum Distance
+    points = np.array([np.argmin(i) for i in distances])  # Step 3
+
+    # Repeating the above steps for a defined number of iterations
+    # Step 4
+    for _ in range(no_of_iterations):
+        centroids = []
+        for idx in range(k):
+            # Updating Centroids by taking mean of Cluster it belongs to
+            temp_cent = x[points == idx].mean(axis=0)
+            centroids.append(temp_cent)
+
+        centroids = np.vstack(centroids)  # Updated Centroids
+
+        distances = cdist(x, centroids, 'euclidean')
+        points = np.array([np.argmin(i) for i in distances])
+
+    return points
